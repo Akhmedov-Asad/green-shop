78c1464d5a48782ec20691c2fbd89145948f2911
Akhmedov-Asad <akhmedovasadbek76@gmail.com>
2022-12-01 12:21:32 +0500

    greenshop

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("close-btn");
+
+
+const openBtn = document.getElementById("open-btn");
+
+openBtn.addEventListener("click", () {
+   modal.style.display = "flex";
+})
